189573cb8042b5f6a64b9e70d2bec0662f73fe4f
duncsully <duncsully@gmail.com>
2026-01-07 19:27:55 -0800

    feat: add m3e and config prettier

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,16 @@
-import { render } from "lit-html";
-import "./style.css";
-import { html } from "solit-html";
+import { render } from 'lit-html'
+import { html } from 'solit-html'
+import './style.css'
+import '@m3e/app-bar'
+import '@m3e/theme'
 
-const mount = document.querySelector<HTMLDivElement>("#app")!;
-render(App(), mount);
+const mount = document.querySelector<HTMLDivElement>('#app')!
+render(App(), mount)
 
 function App() {
-  return html`<span>Works!</span>`;
+  return html`<m3e-theme>
+    <m3e-app-bar>
+      <span slot="title">Dodoc</span>
+    </m3e-app-bar>
+  </m3e-theme>`
 }
