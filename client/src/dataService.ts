d333485751cbd91b24ae0aadbe8747a5b3434045
duncsully <duncsully@gmail.com>
2026-01-28 20:42:40 -0800

    feat: utils and data loading functions

--- client/src/dataService.ts
+++ client/src/dataService.ts
@@ -1,10 +1,19 @@
 // Abstraction for all data fetching and mutations
 
 import Pocketbase from 'pocketbase'
-import type { TypedPocketBase } from './pb_types'
-import { state } from 'solit-html'
+import type {
+  TypedPocketBase,
+  DocumentsRecord,
+  Update,
+  Collections,
+  Create,
+} from './pb_types'
+import { effect, memo, state } from 'solit-html'
 
-const pb = new Pocketbase('http://127.0.0.1:8090') as TypedPocketBase
+const pbBaseUrl = import.meta.env.DEV
+  ? 'http://127.0.0.1:8090'
+  : window.location.origin
+const pb = new Pocketbase(pbBaseUrl) as TypedPocketBase
 
 const [_isLoggedIn, setIsLoggedIn] = state(false)
 pb.authStore.onChange(() => {
@@ -28,4 +37,72 @@ export const login = async (email: string, password: string) => {
 
 export const logout = () => {
   pb.authStore.clear()
+}
+
+// Document management --------------------------------------------------------
+// For now, state management strategy is:
+// * Keep a global list of all documents in memory
+// * Do initial load when useSyncDocuments is first called
+// * Also subscribe to changes and remain subscribed while app is running
+const [documents, setDocuments] = state<DocumentsRecord[]>([])
+
+/**
+ * Loads all documents and keeps them in sync via realtime subscriptions.
+ * Should be called once after authenticating.
+ */
+export function useSyncDocuments() {
+  if (!documents().length) {
+    pb.collection('documents')
+      .getFullList({
+        sort: '-created',
+      })
+      .then(setDocuments)
+  }
+
+  effect(function subscribeToDocuments() {
+    pb.collection('documents').subscribe('*', (e) => {
+      const docs = documents(false)
+      const index = docs.findIndex((d) => d.id === e.record.id)
+
+      let newDocs = [...docs]
+      if (e.action === 'create') {
+        newDocs.unshift(e.record)
+      } else if (e.action === 'update' && index !== -1) {
+        newDocs[index] = e.record
+      } else if (e.action === 'delete' && index !== -1) {
+        newDocs.splice(index, 1)
+      }
+      setDocuments(newDocs)
+    })
+
+    return () => {
+      pb.collection('documents').unsubscribe('*')
+      setDocuments([])
+    }
+  })
+}
+
+export function useDocuments() {
+  return documents
+}
+
+export function useDocument(id: () => string) {
+  return memo(() => documents().find((d) => d.id === id()))
+}
+
+export function createDocument(data: Create<Collections.Documents>) {
+  return pb
+    .collection('documents')
+    .create({ ...data, owner: pb.authStore.record?.id })
+}
+
+export function updateDocument(
+  id: string,
+  data: Update<Collections.Documents>
+) {
+  return pb.collection('documents').update(id, data)
+}
+
+export function deleteDocument(id: string) {
+  return pb.collection('documents').delete(id)
 }
--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,7 +1,74 @@
+import {
+  alertController,
+  toastController,
+  type AlertOptions,
+  type RouterDirection,
+} from '@ionic/core'
+import { render, type TemplateResult } from 'lit-html'
+import { signal } from 'solit-html'
+
 /**
  * Factory to create event handlers that extract the value from input events,
  * passing it to the provided callback.
  */
 export function withEventValue(cb: (value: string) => void) {
   return (e: Event) => cb(e.target ? (e.target as HTMLInputElement).value : '')
+}
+
+export async function showToast(message: string) {
+  const toast = await toastController.create({
+    message,
+    duration: 2000,
+    position: 'bottom',
+    buttons: [
+      {
+        text: 'OK',
+        role: 'cancel',
+      },
+    ],
+  })
+  return toast.present()
+}
+
+export async function showAlert(message: string, options?: AlertOptions) {
+  const alert = await alertController.create({
+    message,
+    buttons: [
+      {
+        text: 'OK',
+        role: 'cancel',
+      },
+    ],
+    ...options,
+  })
+  return alert.present()
+}
+
+let tagCounter = 0
+
+/**
+ * Due to Ionic's web component routing, we need to define custom elements for each route.
+ * This utility creates a custom element from a solit-html component function.
+ * @returns The generated tag name of the custom element.
+ */
+export function customElementFrom(component: (props: any) => TemplateResult) {
+  class CustomElement extends HTMLElement {
+    // TODO: generalize for multiple props. For now this is the only expected route param
+    idSig = signal('')
+    set id(value: string) {
+      this.idSig.value = value
+    }
+    connectedCallback() {
+      render(component({ id: this.idSig.get }), this)
+    }
+  }
+  let tagName = `custom-element-${tagCounter++}`
+  customElements.define(tagName, CustomElement)
+  return tagName
+}
+
+// TODO: more graceful method of accessing the router
+// TODO: Also support relative routing?
+export function navigate(url: string, direction: RouterDirection = 'forward') {
+  document.querySelector('ion-router')?.push(url, direction)
 }
